094f3ec3c35ab7cd87a39236157d982f0611ce9a
Hasan Bilgin <hbilgin@gtu.edu.tr>
2016-11-29 12:52:46 +0200

    Add files via upload

---
+++ arduino/servo/BallOnPlate_HardwareDemo/BallOnPlate_HardwareDemo.ino
@@ -0,0 +1,55 @@
+#include <Servo.h>
+
+Servo servoLongEdge;  // This servo used to touchscreen's long edge top or down movement
+Servo servoShortEdge;  // This servo used to touchscreen's long edge top or down movement
+
+  int i = 0;
+
+void setup() {
+  servoLongEdge.attach(10);  // attaches the servo on pin 9 to the servo object
+  servoShortEdge.attach(9); // attaches the servo on pin 10 to the servo object
+}
+
+void loop() {
+
+ // fourCornerDemo();
+ reset();
+}
+
+/*
+ * Set two servos to specific angle
+ * l: Long edge angle
+ * s: Short edge angle
+ */
+void setServoLongShort(int l, int s){
+  servoLongEdge.write(l);
+  servoShortEdge.write(s);
+}  
+
+/*
+ * Reset plate
+ */
+void reset(){
+  servoLongEdge.write(75);
+  servoShortEdge.write(80);
+}
+
+void fourCornerDemo(){
+  servoLongEdge.write(0);
+  servoShortEdge.write(0);
+  delay(1000);
+  
+
+  servoLongEdge.write(0);
+  servoShortEdge.write(170);
+  delay(1000);
+
+  servoLongEdge.write(170);
+  servoShortEdge.write(0);
+  delay(1000);
+
+  servoLongEdge.write(170);
+  servoShortEdge.write(170);
+  delay(1000); 
+}
+
---
+++ arduino/servo/BallOnPlate_HardwareDemo/TouchScreenDemo/TouchScreenDemo.ino
@@ -0,0 +1,55 @@
+/*=================================
+This code demostrates 4-Wire Touch screen 
+interfacing with Arduino
+
+blog.circuits4you.com
+www.circuits4you.com
+
+4- Wire Touchscreen Connections
+A0=====X+
+A1=====X-
+A2=====Y+
+A3=====Y-
+=================================*/
+//Define your Touch screen connections
+#define A0 X1
+#define A1 X2
+#define    Y1
+#define A3 Y2
+//Define your screen resolution as per your Touch screen (Max: 1024)
+#define Xresolution 320 //128
+#define Yresolution 240 //64
+
+void setup()
+{
+   Serial.begin(9600);
+}
+
+void loop()
+{
+  int X,Y; //Touch Coordinates are stored in X,Y variable
+   pinMode(A0,INPUT);
+   pinMode(Y2,INPUT);  
+   digitalWrite(Y2,LOW);
+   pinMode(A0,OUTPUT);
+   digitalWrite(A0,HIGH);
+   pinMode(A1,OUTPUT);
+   digitalWrite(A1,LOW);
+   X = (analogRead(Y1))/(1024/Xresolution); //Reads X axis touch position
+    
+   pinMode(A0,INPUT);
+   pinMode(A1,INPUT);
+   digitalWrite(A1,LOW);
+   pinMode(Y1,OUTPUT);
+   digitalWrite(Y1,HIGH);
+   pinMode(Y2,OUTPUT);
+   digitalWrite(Y2,LOW);
+   Y = (analogRead(A0))/(1024/Yresolution); //Reads Y axis touch position
+  
+  //Display X and Y on Serial Monitor
+   Serial.print("X = ");  
+   Serial.print(X);
+   Serial.print(" Y = ");
+   Serial.println(Y);
+   delay(100);
+}
