9e037b8e9413fea5dc87baa73636226fbdeff9f9
alozta <alozta@gmail.com>
2016-12-21 13:34:57 +0300

    Communication fixes

--- arduino/communication/windows/communication/send-receive/rs232/demo_rx.c
+++ arduino/communication/windows/communication/send-receive/rs232/demo_rx.c
@@ -8,10 +8,13 @@ exit the program by pressing Ctrl-C
 
 compile with the command: gcc demo_rx.c rs232.c -Wall -Wextra -o2 -o test_rx
 
+received string format: <x y servo1Degree servo2Degree>
+
 **************************************************/
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #ifdef _WIN32
 #include "windows.h"
@@ -21,15 +24,19 @@ compile with the command: gcc demo_rx.c rs232.c -Wall -Wextra -o2 -o test_rx
 
 #include "rs232.h"
 
+#define MSG_LEN 20
 
+void delay(int milliseconds);
 
 int main()
 {
-  int i, n,
+  int n=0,
       cport_nr=5,        // change port no: http://www.teuniz.net/RS-232/
       bdrate=9600;       // 9600 baud
+  //int iRx=0;
 
-  unsigned char buf[4096];
+  unsigned char msgRx[MSG_LEN];
+  //char c;
 
   char mode[]={'8','E','2',0};  // change mode http://www.teuniz.net/RS-232/
 
@@ -43,21 +50,32 @@ int main()
 
   while(1)
   {
-    n = RS232_PollComport(cport_nr, buf, 4095);
+    n = RS232_PollComport(cport_nr, msgRx, MSG_LEN);
+    delay(1000);
+    printf("%d\n", n);
 
-    if(n > 0)
-    {
-      buf[n] = 0;   /* always put a "null" at the end of a string! */
+    if(n>0){
+      for(int i=0; i<n; ++i){
+        printf("<%d>", (int)msgRx[i]);
+      }
+      printf("\n");
+      //printf("%c\n", c);
+      /*if(c == 0){
+        msgRx[iRx] = '\0';
+        printf("received %i bytes: <%s>\n", n, (char *)msgRx);
+        sprintf(msgRx, "");
+      }else{
+        msgRx[iRx++] = c;
+      }*/
 
-      for(i=0; i < n; i++)
-      {
-        if(buf[i] < 32)  /* replace unreadable control-codes by dots */
-        {
-          buf[i] = '.';
-        }
+      if(n >= MSG_LEN-1){
+        msgRx[MSG_LEN-1] = '\0';
+      }else{
+        msgRx[n] = '\0';
+        printf("received %i bytes: <%s>\n", n, (char *)msgRx);
       }
 
-      printf("received %i bytes: %s\n", n, (char *)buf);
+      for(int i=0; i<n; ++i) msgRx[i] = '\0';
     }
 
 #ifdef _WIN32
@@ -68,5 +86,17 @@ int main()
   }
 
   return(0);
+}
+
+
+void delay(int milliseconds)
+{
+    long pause;
+    clock_t now,then;
+
+    pause = milliseconds*(CLOCKS_PER_SEC/1000);
+    now = then = clock();
+    while( (now-then) < pause )
+        now = clock();
 }
 
--- arduino/communication/windows/communication/send-receive/rs232/demo_tx.c
+++ arduino/communication/windows/communication/send-receive/rs232/demo_tx.c
@@ -8,6 +8,8 @@ exit the program by pressing Ctrl-C
 
 compile with the command: gcc demo_tx.c rs232.c -Wall -Wextra -o2 -o test_tx
 
+Transmitted string format: <setPointX setPointY>
+
 **************************************************/
 
 #include <stdlib.h>
@@ -21,21 +23,20 @@ compile with the command: gcc demo_tx.c rs232.c -Wall -Wextra -o2 -o test_tx
 
 #include "rs232.h"
 
-
+#define MSG_LEN 20
 
 int main()
 {
-  int i=0,
-      cport_nr=5,        // change port no: http://www.teuniz.net/RS-232/
+  int cport_nr=5,        // change port no: http://www.teuniz.net/RS-232/
       bdrate=9600;       // 9600 baud
+  int setPointX, setPointY;
 
-  char mode[]={'8','E','2',0},  // change mode http://www.teuniz.net/RS-232/
-       str[2][512];
+  char mode[]={'8','E','2',0};  // change mode http://www.teuniz.net/RS-232/
+  char msgTx[MSG_LEN];
 
-
-  strcpy(str[0], "The quick brown fox jumped over the lazy grey dog.\n");
-
-  strcpy(str[1], "Happy serial programming!\n");
+  setPointX = 200;
+  setPointY = 150;        //center
+  sprintf(msgTx, "%d %d", setPointX, setPointY);
 
   if(RS232_OpenComport(cport_nr, bdrate, mode))
   {
@@ -46,9 +47,9 @@ int main()
 
   while(1)
   {
-    RS232_cputs(cport_nr, str[i]);
+    RS232_cputs(cport_nr, msgTx);
 
-    printf("sent: %s\n", str[i]);
+    printf("sent: %s\n", msgTx);
 
 #ifdef _WIN32
     Sleep(1000);
@@ -56,9 +57,6 @@ int main()
     usleep(1000000);  /* sleep for 1 Second */
 #endif
 
-    i++;
-
-    i %= 2;
   }
 
   return(0);
