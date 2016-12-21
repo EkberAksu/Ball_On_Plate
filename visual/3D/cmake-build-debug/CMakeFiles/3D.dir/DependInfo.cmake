73d305b56ac66b64e9a867c135fd86f81814fe6a
Şahin Eğilmez <SahinEgilmez@users.noreply.github.com>
2016-12-22 00:48:45 +0200

    Ekrandan değerler alınıp 3D olarak simule ediliyor
    Bunun için BAP_FINAL arduino dosyasını kullan!!

---
+++ visual/3D/cmake-build-debug/CMakeFiles/3D.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Programlama/Proje1/395_Project1/visual/3D/main.cpp" "E:/Programlama/Proje1/395_Project1/visual/3D/cmake-build-debug/CMakeFiles/3D.dir/main.cpp.obj"
+  "E:/Programlama/Proje1/395_Project1/visual/3D/rs232.cpp" "E:/Programlama/Proje1/395_Project1/visual/3D/cmake-build-debug/CMakeFiles/3D.dir/rs232.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files (x86)/OpenSceneGraph/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ visual/3D/cmake-build-debug/CMakeFiles/3D.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3D.dir/main.cpp.obj"
+  "CMakeFiles/3D.dir/rs232.cpp.obj"
+  "3D.pdb"
+  "3D.exe"
+  "3D.exe.manifest"
+  "lib3D.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
