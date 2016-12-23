99a1db4bde5adf89ac2a6071c683e492befed2b6
Şahin Eğilmez <SahinEgilmez@users.noreply.github.com>
2016-12-23 05:29:39 +0200

    Final demosu için..

---
+++ visual/Ball&Plate/cmake-build-debug/CMakeFiles/Ball_Plate.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ball_Plate.dir/main.cpp.obj"
+  "CMakeFiles/Ball_Plate.dir/rs232.cpp.obj"
+  "Ball_Plate.pdb"
+  "Ball_Plate.exe"
+  "Ball_Plate.exe.manifest"
+  "libBall_Plate.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ball_Plate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
